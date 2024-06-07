33f10d6f0cc9446dd5c921ae3dc747684f2b9197
Ahmed Ghonim <35805998+ahmedghonim@users.noreply.github.com>
2024-06-07 18:41:29 +0300

    feat: Add logo and contact information to contact us page

--- app/[lang]/(public)/contact-us/page.tsx
+++ app/[lang]/(public)/contact-us/page.tsx
@@ -2,7 +2,8 @@ import { Text } from "@/ui/atoms";
 import ContactForm from "@/view/contact/contact-form";
 import { getTranslations } from "next-intl/server";
 import React from "react";
-
+import Logo from "@/assets/images/logo.png";
+import Image from "next/image";
 async function Page() {
   const t = await getTranslations("common");
   return (
@@ -13,6 +14,22 @@ async function Page() {
       <div className="mx-[15%] space-y-4">
         <Text as="h2">{t("send_us_message")}</Text>
         <ContactForm />
+      </div>
+      <hr />
+      <Text as="h2" className="text-wrap w-[380px]">
+        {t("find_us")}
+      </Text>
+      <div className="mx-[15%] space-y-4">
+        <ul className="space-y-4">
+          <li className=" flex gap-10 items-center">
+            <Image src={Logo} width={100} height={100} alt="logo" />
+            <Text as="p">{t("phone")}: 966551352319 </Text>
+          </li>
+          <li className=" flex gap-10 items-center">
+            <Image src={Logo} width={100} height={100} alt="logo" />
+            <Text as="p">{t("email")}: thelal.studio@gmail.com </Text>
+          </li>
+        </ul>
       </div>
     </div>
   );
