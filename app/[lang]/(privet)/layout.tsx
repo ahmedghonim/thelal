b7745f218bd18790553dde5e3bf7ee3efbb3baf9
Ahmed Ghonim <ahmed.ghonim.dev@gmail.com>
2024-07-22 22:11:43 +0300

    feat: Add lazy loading for images and update form components

--- app/[lang]/(privet)/layout.tsx
+++ app/[lang]/(privet)/layout.tsx
@@ -7,7 +7,7 @@ function Layout({ children }: { children: React.ReactNode }) {
   const t = useTranslations("common");
   const pages = ["design", "build", "contact", "our-blog", "team"];
   return (
-    <div className="flex min-h-screen">
+    <div className="flex min-h-screen overflow-y-auto">
       <div className="w-[300px] bg-black/80 h-screen">
         <Image src={Logo} alt="logo" className="p-12" />
         <div className="flex flex-col gap-2 p-10 text-white font-bold text-lg ">
