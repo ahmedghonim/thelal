96a85d94a1a0816face9f4495455640bf694dede
Ahmed Ghonim <ahmed.ghonim.dev@gmail.com>
2024-11-24 21:28:30 +0200

    fix: Remove contact us title from the contact page for cleaner layout

--- app/[lang]/(public)/contact-us/page.tsx
+++ app/[lang]/(public)/contact-us/page.tsx
@@ -65,9 +65,6 @@ async function Page() {
   const t = await getTranslations("common");
   return (
     <div className="pt-10 space-y-10">
-      <Text as="h1" className="text-wrap w-[380px]">
-        {t("contact_us_title")}
-      </Text>
       <div className="mx-[15%] space-y-4">
         <Text as="h2">{t("send_us_message")}</Text>
         <ContactForm />
